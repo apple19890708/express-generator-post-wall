2a2bada1b4b25c43cd3165bf97463a5e665af92c
Tommy <tommychen@applyeasypro.com>
2022-04-27 18:35:54 +0800

    [feat] init

---
+++ routes/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const Post = require("../models/postsModel")
+
+router.get('/', async (req, res, next) => {
+	const post = await Post.find();
+  res.status(200).json({
+    post
+  })
+});
+
+router.post('/', async (req, res, next) => {
+	const newPost = await Post.create({
+		content: req.body.content,
+		image: req.body.image,
+		name: req.body.name
+	})
+	res.status(200).json({
+    status: "success",
+		post: newPost
+  })
+})
+
+router.patch('/:id', async (req, res, next) => {
+	console.log('req.params.id', req.params.id)
+	const posts = await Post.findByIdAndUpdate(
+		req.params.id, 
+		{
+			content: req.body.content,
+			image: req.body.image,
+			name: req.body.name
+		},
+		{
+			returnDocument: 'after',
+		}
+	);
+	res.status(200).json({
+		status: "success",
+		post: posts
+	})
+})
+
+module.exports = router;
