20a3131123e49ce3670214a08572bd1d6a6b05d7
Tommy <tommychen@applyeasypro.com>
2022-04-29 14:23:20 +0800

    [feat] add error methods

--- app.js
+++ app.js
@@ -37,24 +37,43 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 可做為路由守衛使用
+app.use(function(req,res,next) {
+	console.log('驗證通過');
+	next();
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts', postsRouter)
+app.use('/posts', postsRouter);
+
+// error catch 404
+
+app.use(function(req, res, next) {
+	res.status(404).send('抱歉，您找的頁面不存在')
+})
+
+// error catch 500
+
+app.use(function(err, req, res, next) {
+	console.log('err', err.stack)
+	res.status(500).send('程式發生問題，請稍後再試')
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
