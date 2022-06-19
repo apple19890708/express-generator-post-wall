b3be7e21301a6d5041b5d7a7843c63d057a04211
Tommy <tommychen@applyeasypro.com>
2022-06-19 20:15:56 +0800

    [feat] add thirdPartySignIn isLogin type

--- service/thirdPartySignIn.js
+++ service/thirdPartySignIn.js
@@ -24,9 +24,8 @@ const thirdPartySignIn = async (thirdPartyName, data, res) => {
       } else if (userExisted.activeStatus === 'meta') {
         userStateData = { activeStatus: 'both' };
       }
-      userStateData[key] = id; //為相對應的第三方登入加上ID
+      userStateData[key] = `${id}6666`; //為相對應的第三方登入加上ID
 			userStateData['isLogin'] = true;
-			console.log('userStateData', userStateData)
       await User.updateOne({ email }, userStateData);
 		}
 		user = userExisted;
