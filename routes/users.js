b52f98507652c2a5789f5e8d078dcb2be2572200
Tommy <tommychen@applyeasypro.com>
2022-07-21 16:39:14 +0800

    [bugfix] test1

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 const UsersControllers = require('../controllers/user');
 const handleErrorAsync = require("../service/handleErrorAsync");
-const {isAuth, generateURLJWT} = require('../service/auth');
+const { generateURLJWT} = require('../service/auth');
 const passport = require('passport');
 const upload = require('../service/image');
 
 router.get('/', handleErrorAsync(UsersControllers.getUsers));
 router.post('/sign_up', handleErrorAsync(UsersControllers.signUpUser));
 router.post('/sign_in', handleErrorAsync(UsersControllers.signInUser));
-router.post('/sign_out', isAuth, handleErrorAsync(UsersControllers.signOutUser));
-router.get('/profile/', isAuth, handleErrorAsync(UsersControllers.getUsersProfile));
-router.get('/:id', isAuth, handleErrorAsync(UsersControllers.getOtherUsersProfile));
-router.patch('/profile/', isAuth, upload, handleErrorAsync(UsersControllers.updateUsersProfile));
-router.post('/updatePassword', isAuth, handleErrorAsync(UsersControllers.updatePassword));
-router.get('/getLikeList', isAuth, handleErrorAsync(UsersControllers.getLikeList));
-router.get('/getUserCheck', isAuth, handleErrorAsync(UsersControllers.getUserCheck));
-router.post('/:id/follow', isAuth, handleErrorAsync(UsersControllers.addFollowr));
-router.delete('/:id/unfollow', isAuth, handleErrorAsync(UsersControllers.cancelFollowing));
-router.get('/following', isAuth, handleErrorAsync(UsersControllers.getUserFollowing));
+router.post('/sign_out',  handleErrorAsync(UsersControllers.signOutUser));
+router.get('/profile/',  handleErrorAsync(UsersControllers.getUsersProfile));
+router.get('/:id',  handleErrorAsync(UsersControllers.getOtherUsersProfile));
+router.patch('/profile/',  upload, handleErrorAsync(UsersControllers.updateUsersProfile));
+router.post('/updatePassword',  handleErrorAsync(UsersControllers.updatePassword));
+router.get('/getLikeList',  handleErrorAsync(UsersControllers.getLikeList));
+router.get('/getUserCheck',  handleErrorAsync(UsersControllers.getUserCheck));
+router.post('/:id/follow',  handleErrorAsync(UsersControllers.addFollowr));
+router.delete('/:id/unfollow',  handleErrorAsync(UsersControllers.cancelFollowing));
+router.get('/following',  handleErrorAsync(UsersControllers.getUserFollowing));
 // google登入
 router.get('/google', passport.authenticate('google', {
 	scope: ['email', 'profile']
